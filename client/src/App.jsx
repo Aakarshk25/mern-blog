15065b476fd6c847de34ffe9c99404f568bf1926
Aakarshk25 <aakarshkumar495@gmail.com>
2025-01-17 18:55:39 +0530

    creating nav and header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Signin from './Pages/Signin'
 import SignUp from './Pages/SignUp'
 import Dashboard from './Pages/Dashboard'
 import Projects from './Pages/Projects'
+import Header from './components/Header'
+
 
 
 
@@ -12,6 +14,7 @@ import Projects from './Pages/Projects'
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/about" element={<About />}></Route>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,12 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  //  'node_modules/flowbite-react/**/*{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
+  // plugins: [require('flowbite/plugin')],
   plugins: [],
 }
